7db8b5ec397b0dbbbc4cb31aee75f5b4e850c985
Vagan Arakelyan <vra.vagan@gmail.com>
2023-10-25 22:50:49 +0200

    v2.0

--- src/components/Preloader.js
+++ src/components/Preloader.js
@@ -1,17 +1,32 @@
 import React from "react";
 
 function Preloader({ progress }) {
+    
+    let progressText = "Отрисовываем сцены";
+
+    if (progress >= 25) {
+        progressText = "Собираем мир из пикселей";
+    }
+    
+    if (progress >= 50) {
+        progressText = "Драконы и гномы в сборе";
+    }
+    
+    if (progress >= 75) {
+        progressText = "Складываем пиксели в историю";
+    }
+
     return (
         <div className="preloader">
             <div className="progressBar">
                 <div className="progress" style={{ width: `${progress}%` }}></div>
             </div>
             <div className="progressText">
-                <span>Отрисовываем сцены</span>
-                <span>Творим магию</span>
+                <div>{progressText}</div>
             </div>
         </div>
     );
 }
+
 
 export default Preloader;
